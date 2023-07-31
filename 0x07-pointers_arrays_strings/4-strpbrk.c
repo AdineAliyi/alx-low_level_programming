ecda23f4e3bad0b323217f647c88f6e66a86126c
AdineAliyi <adinewalisaid@gmail.com>
2023-07-31 15:48:44 -0700

    A function to search a string in for any set of byte

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,26 +7,21 @@
   * @accept: the set of bytes to search for
   * Return: a pointer to the byte in s that matches a byte in accept
   */
-
 char *_strpbrk(char *s, char *accept)
 {
-	int in_s = 0, in_a;
-	char *ptr_to_match = NULL;
+	int i, j;
 
-	/* loop through s to the end */
-	while (s[in_s])
+	i = 0;
+	while (s[i] != '\0')
 	{
-	
-		for (in_a = 0; accept[in_a]; in_a++)
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			/* find a match, get its address, break */
-			if (s[in_s] == accept[in_a])
-			{
-				ptr_to_match = &s[in_s];
-				return (ptr_to_match);
-			}
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
 		}
-		in_s++;
+		i++;
 	}
-	return (ptr_to_match);
+	return ('\0');
 }
