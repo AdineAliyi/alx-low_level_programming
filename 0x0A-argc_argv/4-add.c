741a3ceaf7dbfd835531784502ccf62928b1075f
AdineAliyi <adinewalisaid@gmail.com>
2023-08-08 12:25:53 -0700

    A program to add +ve numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,47 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
-  * main - To add Positive numbers
-  * @argc: number of args
-  * @argv: array of args
-  * Return: 0.
-  */
+ * main - Add Positive numbers
+ * @argc: The Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if there is no integer in the passed in arguments, 0 otherwise
+ */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, length, sum;
+	char *ptr;
 
-	/* if arguments are present */
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			/* if digit */
-			if (isdigit(*argv[i]))
-			{
-				/* als o a digit */
-				sum += atoi(argv[i]);
-
-				/* in the end */
-				if (i == argc - 1)
-				{
-					printf("%d\n", sum);
-				}
-			}
-			else
-			{
-				/* if non-digit */
-				printf("Error\n");
-				return (1);
-			}
-		}
-	}
+	if (argc < 2)
+		printf("0\n");
 	else
 	{
-		/* no numbers */
-		printf("0\n");
-	}
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
 
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[i]);
+		}
+
+	printf("%d\n", sum);
+	}
 	return (0);
 }
