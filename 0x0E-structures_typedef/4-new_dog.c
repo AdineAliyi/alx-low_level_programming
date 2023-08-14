e8142f42096d56ac33640d455390fd32de2ec95a
AdineAliyi <adinewalisaid@gmail.com>
2023-08-14 12:26:25 -0700

    A function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,9 +23,8 @@ return (NULL);
 }
 
 for (lname = 0; name[lname]; lname++)
-		
 for (lowner = 0; owner[lowner]; lowner++)
-		
+
 p_dog->name = malloc(lname + 1);
 p_dog->owner = malloc(lowner + 1);
 
